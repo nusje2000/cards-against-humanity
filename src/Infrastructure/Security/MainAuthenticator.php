a92312abff6036190b5c934324889e0f0f0f8908
Miauwdy <maudy@varias.io>
2023-10-16 16:56:00 +0200

    WIP: bit cleanup of the mainAuthenticator

--- src/Infrastructure/Security/MainAuthenticator.php
+++ src/Infrastructure/Security/MainAuthenticator.php
@@ -5,34 +5,24 @@ declare(strict_types=1);
 namespace Nusje2000\CAH\Infrastructure\Security;
 
 use Nusje2000\CAH\Domain\Exception\Game\PlayerDoesNotExist;
-use Nusje2000\CAH\Domain\Player\Username;
 use Nusje2000\CAH\Infrastructure\Entity\User;
 use Nusje2000\CAH\Infrastructure\Model\LoginCredentials;
 use Nusje2000\CAH\Infrastructure\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
-use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfToken;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 final class MainAuthenticator extends AbstractLoginFormAuthenticator
@@ -42,17 +32,10 @@ final class MainAuthenticator extends AbstractLoginFormAuthenticator
     public function __construct(
         private readonly UserRepository $userRepository,
         private readonly UrlGeneratorInterface $urlGenerator,
-        private readonly CsrfTokenManagerInterface $csrfTokenManager,
-        private readonly UserPasswordHasherInterface $passwordHasher,
         private readonly RouterInterface $router,
 
 ) {}
-
-    public function supports(Request $request): bool
-    {
-        return ($request->getPathInfo() === '/login' && $request->isMethod('POST'));;
-    }
-
+    
     public function getCredentials(Request $request): LoginCredentials
     {
         $credentials = LoginCredentials::fromRequest($request);
@@ -110,13 +93,5 @@ final class MainAuthenticator extends AbstractLoginFormAuthenticator
     protected function getLoginUrl(Request $request): string
     {
         return $this->urlGenerator->generate('cah_index');
-    }
-
-    private function validateCsrfToken(LoginCredentials $credentials): void
-    {
-        $token = new CsrfToken('authenticate', $credentials->csrfToken());
-        if (!$this->csrfTokenManager->isTokenValid($token)) {
-            throw new InvalidCsrfTokenException('Invalid csrf token.');
-        }
     }
 }
